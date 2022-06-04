90c360c4708cb2234119bca2e95bb95e01c6fdb8
Aliko Tektov <70585781+Aliko-XIII@users.noreply.github.com>
2022-06-04 14:51:08 +0300

    Add files via upload

---
+++ PR3/PR3/Administration.xaml.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+
+
+namespace PR3
+{
+    /// <summary>
+    /// Логика взаимодействия для Administration.xaml
+    /// </summary>
+    public partial class Administration : Window
+    {
+        string connectionString = null;
+        SqlConnection connection = null;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        DataTable dT;
+        int LenTable;
+        int index;
+
+        public Administration()
+        {
+            InitializeComponent();            
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            UpdateDataTable();
+            RealAdminPasswd.Password = ""; RealAdminPasswd.IsEnabled = false;
+            NewAdminPasswd.Password = ""; NewAdminPasswd.IsEnabled = false;
+            NewAdminPasswd2.Password = ""; NewAdminPasswd2.IsEnabled = false;
+            PrevButton.IsEnabled = false; NextButton.IsEnabled = false;
+            UpdatePasswdButton.IsEnabled = false;
+            AddUserButton.IsEnabled = false;
+            CorrectStatusBtn.IsEnabled = false;
+            CorrectRestrictionBtn.IsEnabled = false;
+            dT.Clear();
+            dataGrid.ItemsSource = dT.DefaultView;
+            AdminPasswd.Password = "";
+            UsersLogins.ItemsSource = "";
+        }
+
+        private void UpdatePasswd_Click(object sender, RoutedEventArgs e)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            String strQ;
+            String RealPass = RealAdminPasswd.Password.ToString();
+            String NewPass = NewAdminPasswd.Password.ToString();
+            String NewPass2 = NewAdminPasswd2.Password.ToString();
+            if ((RealPass == AdminPasswd.Password.ToString()) 
+            && (NewPass != String.Empty)
+            && (NewPass == NewPass2))
+
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    strQ = "UPDATE MainTable SET Password ='" + NewPass + "'WHERE Login = 'ADMIN'; ";
+                    command = new SqlCommand(strQ, connection);
+                    command.ExecuteNonQuery();
+                }
+            }
+            connection.Close();
+            UpdateDataTable();
+        }
+
+        void UpdateDataTable()
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                adapter = new SqlDataAdapter("SELECT Name, Surname, Login, Status, Restriction FROM MainTable", connection);
+                dT = new DataTable("Користувачі системи");
+                adapter.Fill(dT);
+                dataGrid.ItemsSource = dT.DefaultView;
+                LenTable = dT.Rows.Count;
+            }
+            connection.Close();
+        }
+        private void Prev_Click(object sender, RoutedEventArgs e)
+        {
+            if (index > 0)
+            {
+                index--;
+                UserNameSelected.Content = dT.Rows[index][0].ToString();
+                UserSurnameSelected.Content = dT.Rows[index][1].ToString();
+                UserLoginSelected.Content = dT.Rows[index][2].ToString();
+                UserStatusSelected.Content = dT.Rows[index][3].ToString();
+                ChangeActive.IsChecked = Boolean.Parse(dT.Rows[index][3].ToString().ToLower());
+                UserRestrictionSelected.Content = dT.Rows[index][4].ToString();
+                ChangeRestriction.IsChecked = Boolean.Parse(dT.Rows[index][4].ToString().ToLower());
+
+            }
+        }
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            if (index < LenTable - 1)
+            {
+                index++;
+                UserNameSelected.Content = dT.Rows[index][0].ToString();
+                UserSurnameSelected.Content = dT.Rows[index][1].ToString();
+                UserLoginSelected.Content = dT.Rows[index][2].ToString();
+                UserStatusSelected.Content = dT.Rows[index][3].ToString();
+                ChangeActive.IsChecked = Boolean.Parse(dT.Rows[index][3].ToString().ToLower());
+                UserRestrictionSelected.Content = dT.Rows[index][4].ToString();
+                ChangeRestriction.IsChecked = Boolean.Parse(dT.Rows[index][4].ToString().ToLower());
+
+            }
+        }
+
+        private void AddUser_Click(object sender, RoutedEventArgs e)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            String strQ;
+            String UserLogin = AddingUserLogin.Text;
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    strQ = "INSERT INTO MainTable (Name, Surname, Login, Status,Restriction) values('', '', '" + UserLogin + "', 1, 0); ";
+                    command = new SqlCommand(strQ, connection);
+                    command.ExecuteNonQuery();
+                }
+                UpdateDataTable();
+                SetUserList();
+            }
+            catch
+            {
+                MessageBox.Show("Користувача не додано! Можливо такий в базі вже є!");
+            }
+            connection.Close();
+            UpdateDataTable();
+
+        }
+
+        private void CorrectStatusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            String strQ;
+            bool UserStatus = (bool)ChangeActive.IsChecked;
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                strQ = "UPDATE MainTable SET Status ='" + UserStatus + "' WHERE Login='" +
+                UsersLogins.SelectedValue.ToString() + "';";
+                command = new SqlCommand(strQ, connection);
+                command.ExecuteNonQuery();
+            }
+            connection.Close();
+            UpdateDataTable();
+
+        }
+
+        private void CorrectRestrictionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            String strQ;
+            bool UserRestriction = (bool)ChangeRestriction.IsChecked;
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                strQ = "UPDATE MainTable SET Restriction ='" + UserRestriction + "' WHERE Login = '" + UsersLogins.SelectedValue.ToString() + "'; ";
+                command = new SqlCommand(strQ, connection);
+                command.ExecuteNonQuery();
+            }
+            connection.Close();
+            UpdateDataTable();
+        }
+
+        private void ExitFromSystem_Click(object sender, RoutedEventArgs e)
+        {
+            RealAdminPasswd.Password = ""; RealAdminPasswd.IsEnabled = false;
+            NewAdminPasswd.Password = ""; NewAdminPasswd.IsEnabled = false;
+            NewAdminPasswd2.Password = ""; NewAdminPasswd2.IsEnabled = false;
+            PrevButton.IsEnabled = false; NextButton.IsEnabled = false;
+            UpdatePasswdButton.IsEnabled = false;
+            AddUserButton.IsEnabled = false;
+            CorrectStatusBtn.IsEnabled = false;
+            CorrectRestrictionBtn.IsEnabled = false;
+            dT.Clear();
+            dataGrid.ItemsSource = dT.DefaultView;
+            AdminPasswd.Password = "";
+            UsersLogins.ItemsSource = "";
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            MainWindow mw = new MainWindow();
+            mw.Show();
+        }
+
+        void SetUserList()
+        {
+            var data = new DataSet();
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            adapter = new SqlDataAdapter("Select Login From MainTable;", connectionString);
+            adapter.Fill(data);
+            List<string> logins = new List<string>();
+            foreach (DataRow row in data.Tables[0].Rows)
+            {
+                logins.Add(row["Login"].ToString());
+            }
+            connection.Close();
+            UsersLogins.ItemsSource = logins;
+        }
+        private void AuthoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataSet data = new DataSet();
+            adapter = new SqlDataAdapter("Select Password From MainTable WHERE Login='ADMIN';", connectionString);
+            adapter.Fill(data);
+            string password= data.Tables[0].Rows[0]["Password"].ToString();
+            if (AdminPasswd.Password == password)
+            {
+                UpdateDataTable();
+                RealAdminPasswd.IsEnabled = true;
+                NewAdminPasswd.IsEnabled = true;
+                NewAdminPasswd2.IsEnabled = true;
+                PrevButton.IsEnabled = true; NextButton.IsEnabled = true;
+                UpdatePasswdButton.IsEnabled = true;
+                AddUserButton.IsEnabled = true;
+                CorrectStatusBtn.IsEnabled = true;
+                CorrectRestrictionBtn.IsEnabled = true;
+                SetUserList();
+                index =0;
+                UserNameSelected.Content = dT.Rows[index][0].ToString();
+                UserSurnameSelected.Content = dT.Rows[index][1].ToString();
+                UserLoginSelected.Content = dT.Rows[index][2].ToString();
+                UserStatusSelected.Content = dT.Rows[index][3].ToString();
+                UserRestrictionSelected.Content = dT.Rows[index][4].ToString();
+                ChangeActive.IsChecked = Boolean.Parse(dT.Rows[index][3].ToString().ToLower());
+                ChangeRestriction.IsChecked = Boolean.Parse(dT.Rows[index][4].ToString().ToLower());
+            }
+            else
+            {
+                AdminPasswd.Clear();
+                MessageBox.Show("Невірний пароль адміністратора!");
+            }
+
+
+        }
+
+        private void UsersLogins_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            for (int i =0; i < dT.Rows.Count; i++)
+            {
+                if ((string)dT.Rows[i][2] == (string)UsersLogins.SelectedItem)
+                {
+                    index = i;
+                }
+            }
+            UserNameSelected.Content = dT.Rows[index][0].ToString();
+            UserSurnameSelected.Content = dT.Rows[index][1].ToString();
+            UserLoginSelected.Content = dT.Rows[index][2].ToString();
+            UserStatusSelected.Content = dT.Rows[index][3].ToString();
+            ChangeActive.IsChecked = Boolean.Parse(dT.Rows[index][3].ToString().ToLower());
+            UserRestrictionSelected.Content = dT.Rows[index][4].ToString();
+            ChangeRestriction.IsChecked = Boolean.Parse(dT.Rows[index][4].ToString().ToLower());
+        }
+    }
+}
---
+++ PR3/PR3/UserFormWPF.xaml.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+
+namespace PR3
+{
+    /// <summary>
+    /// Логика взаимодействия для UserFormWPF.xaml
+    /// </summary>
+    public partial class UserFormWPF : Window
+    {
+        string login;
+        string passwd;
+        SqlConnection connection=null;
+        string connectionString=null;
+        SqlDataAdapter adapter;
+        DataTable dT;
+        int count;
+        SqlCommand command;
+        public UserFormWPF()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            NewNameField.IsEnabled = false;
+            NewSurnameField.IsEnabled = false;
+            NewPasswdField.IsEnabled = false;
+            NewPasswdField2.IsEnabled = false;
+            UpdateDataBtn.IsEnabled = false;
+        }
+
+        private void AutorBtn_Click(object sender, RoutedEventArgs e)
+        {
+            login = loginField.Text;
+            passwd = passwdField.Password;
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                String strQ = "SELECT * FROM MainTable WHERE Login='" + login + "';";
+                adapter = new SqlDataAdapter(strQ, connection);
+                dT = new DataTable("Користувачі системи");
+                adapter.Fill(dT);
+                if (dT.Rows.Count == 0)
+                {
+                    MessageBoxResult result = MessageBox.Show("Такого користувача на знайдено\nХочете зареєструвати користувача?", "Помилка входу", MessageBoxButton.YesNo);
+                    if(result == MessageBoxResult.No)
+                    {
+                        System.Windows.Application.Current.Shutdown();
+                    }
+                }
+
+                else
+                {
+                    bool Status = Convert.ToBoolean(dT.Rows[0][4]);
+                    if (!Status)
+                        MessageBox.Show("Користувач заблокований Адміністратором системи!");
+                    else
+                    {
+                        if ((bool)dT.Rows[0][5])
+                        {
+                            if ((dT.Rows[0][2].ToString() == login) &&
+
+                        (dT.Rows[0][3].ToString() == passwd))
+
+                            {
+                                NewNameField.Text = dT.Rows[0][0].ToString();
+                                NewSurnameField.Text = dT.Rows[0][1].ToString();
+                                NewPasswdField.Password = "";
+                                NewPasswdField2.Password = "";
+                                NewNameField.IsEnabled = true;
+                                NewSurnameField.IsEnabled = true;
+                                NewPasswdField.IsEnabled = true;
+                                NewPasswdField2.IsEnabled = true;
+                                UpdateDataBtn.IsEnabled = true;
+
+                            }
+                            else
+                            {
+                                count++;
+
+                                String s = "Невірно введений пароль! " +
+
+                                "Помилкове введення No" + count.ToString();
+                                MessageBox.Show(s);
+                                if (count == 3)
+                                    System.Windows.Application.Current.Shutdown();
+                            }
+                        }
+                        else
+                        {
+                            if (dT.Rows[0][2].ToString() == login)
+                            {
+                                NewNameField.Text = dT.Rows[0][0].ToString();
+                                NewSurnameField.Text = dT.Rows[0][1].ToString();
+                                NewPasswdField.Password = "";
+                                NewPasswdField2.Password = "";
+                                NewNameField.IsEnabled = true;
+                                NewSurnameField.IsEnabled = true;
+                                NewPasswdField.IsEnabled = true;
+                                NewPasswdField2.IsEnabled = true;
+                                UpdateDataBtn.IsEnabled = true;
+
+                            }
+
+
+                        }
+                    }
+
+                }
+            }
+            connection.Close();
+        }
+        private void RegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            String nameReg = NameField.Text;
+            String surnameReg = SurnameField.Text;
+            String loginReg = loginRegField.Text;
+            String passwdReg = passwdRegField.Password;
+            String passwdReg2 = passwdRegField2.Password;
+            String strQ;
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                try
+                {
+                    if ((passwdReg == passwdReg2) && (loginReg != "") &&
+                    (passwdReg != "") && RestrictionFuncLogin(loginReg) && RestrictionFunc(passwdReg))
+                    {
+                        strQ = "INSERT INTO MainTable ";
+                        strQ += "VALUES ('" + nameReg + "', '" + surnameReg +
+                        "', '" + loginReg + "', '" + passwdReg + "','True', 'False'); ";
+                        command = new SqlCommand(strQ, connection);
+                        command.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        //MessageBox.Show($"Pass:{passwdReg}; login:{loginReg}; {RestrictionFuncLogin(loginReg)}");
+                        MessageBox.Show("Обліковий запис не створено. Спробуйте ще раз!");
+
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Користувач з таким логіном вже існує у системі!");
+                }
+            }
+            connection.Close();
+        }
+
+        private void CloseBtnSystem_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            NewNameField.Text = ""; NewNameField.IsEnabled = false;
+            NewSurnameField.Text = ""; NewSurnameField.IsEnabled = false;
+            NewPasswdField.Password = ""; NewPasswdField.IsEnabled = false;
+            NewPasswdField2.Password = ""; NewPasswdField2.IsEnabled = false;
+            UpdateDataBtn.IsEnabled = false;
+            passwdField.Password = "";
+        }
+
+        Boolean RestrictionFuncLogin(String Log)
+        {
+            foreach (char c in "\\,./<>?!@\"':;{}[]|#$%^&*()=+")
+            {
+                if (Log.Contains(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        Boolean RestrictionFunc(String Pass)
+        {
+            Byte Count1, Count2, Count3;
+            Byte LenPass = (Byte)Pass.Length;
+            Count1 = Count2 = Count3 = 0;
+            for (Byte i = 0; i < LenPass; i++)
+            {
+                if ((Convert.ToInt32(Pass[i]) >= 65) &&
+
+                (Convert.ToInt32(Pass[i]) <= 65 + 25))
+
+                    Count1++;
+                if ((Convert.ToInt32(Pass[i]) >= 97) &&
+
+                (Convert.ToInt32(Pass[i]) <= 97 + 25))
+
+                    Count2++;
+                if ((Pass[i] == '+') || (Pass[i] == '-') || (Pass[i] == '*') ||
+
+                (Pass[i] == '/'))
+
+                    Count3++;
+            }
+            return (Count1 * Count2 * Count3 != 0);
+        }
+
+        private void UpdateDataBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            String newname = NewNameField.Text;
+            String newsurname = NewSurnameField.Text;
+            String newpasswd = NewPasswdField.Password;
+            String newpasswd2 = NewPasswdField2.Password;
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                String strQ;
+                if (Convert.ToBoolean(dT.Rows[0][5]) == true)
+                {
+                    if (newpasswd == newpasswd2)
+                    {
+                        if ((newpasswd != String.Empty))
+                        {
+                            Boolean flag = RestrictionFunc(newpasswd);
+                            if (flag == true)
+                            {
+                                strQ = "UPDATE MainTable SET Name='" + newname + "', ";
+                                strQ += "Surname='" + newsurname + "', ";
+                                strQ += "Password='" + newpasswd + "' ";
+                                strQ += "WHERE Login='" + login + "';";
+
+                                command = new SqlCommand(strQ, connection);
+                                command.ExecuteNonQuery();
+                            }
+                            else
+                            {
+                                MessageBox.Show("У паролі немає літер верхнього та нижнього регістрів, а також арифметичних операцій! Спробуйте знову!");
+                            }
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Пароль не може бути пустим");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введені паролі не однакові ");
+                    }
+                }
+                else
+                {
+                    if (newpasswd != String.Empty)
+                    {
+                        MessageBox.Show("Обмеження на пароль не встановлено");
+                    }
+                    else
+                    {
+                        strQ = "UPDATE MainTable SET Name='" + newname + "', ";
+                        strQ += "Surname='" + newsurname + "', ";
+                        strQ += "WHERE Login='" + login + "';";
+                    }
+
+                }
+
+            }
+                
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            MainWindow mw = new MainWindow();
+            mw.Show();
+        }
+    }
+    }
